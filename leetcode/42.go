6e5e5f0ca82f615578fd0c8b32326277e6322001
joseph sheng <sjjworkspace@gmail.com>
2025-02-03 20:36:27 +0800

    42. 接雨水(双指针)

---
+++ leetcode/42.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+)
+
+func trap(height []int) int {
+	left := 0
+	right := len(height) - 1
+
+	maxLeft := 0
+	maxRight := 0
+
+	count := 0
+
+	for left < right {
+		if height[left] < height[right] {
+			if height[left] > maxLeft {
+				maxLeft = height[left]
+			} else {
+				count += maxLeft - height[left]
+			}
+			left++
+		} else {
+			if height[right] > maxRight {
+				maxRight = height[right]
+			} else {
+				count += maxRight - height[right]
+			}
+			right--
+		}
+	}
+	return count
+}
+
+func main() {
+	fmt.Println(trap([]int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}))
+	fmt.Println(trap([]int{4, 2, 0, 3, 2, 5}))
+}
