2106f9184af8e1df06ef16384e13ab995dd8cc08
Joseph Sheng <jsheng@opentext.com>
2025-02-06 14:02:44 +0800

    redo 88

--- leetcode/88.go
+++ leetcode/88.go
@@ -3,26 +3,36 @@ package main
 import "fmt"
 
 func merge(nums1 []int, m int, nums2 []int, n int) {
-	p1 := m - 1
-	p2 := n - 1
+	//tail of result
 	tail := m + n - 1
+	//tail of nums1
+	p1 := m - 1
+	//tail of nums2
+	p2 := n - 1
 
-	for ; tail >= 0; tail-- {
-		if p1 == -1 {
-			nums1[tail] = nums2[p2]
-			p2--
-		} else if p2 == -1 {
-			nums1[tail] = nums1[p1]
-			p1--
-		} else if nums1[p1] > nums2[p2] {
-			nums1[tail] = nums1[p1]
-			p1--
-		} else {
+	for tail >= 0 {
+		//这个代码块在前
+		if p1 < 0 && p2 >= 0 {
 			nums1[tail] = nums2[p2]
 			p2--
 		}
-	}
+		if p2 < 0 && p1 >= 0 {
+			nums1[tail] = nums1[p1]
+			p1--
+		}
+		//这个代码块在后，否则p1--和p2--会影响逻辑
+		if p1 >= 0 && p2 >= 0 {
+			if nums1[p1] >= nums2[p2] {
+				nums1[tail] = nums1[p1]
+				p1--
+			} else {
+				nums1[tail] = nums2[p2]
+				p2--
+			}
+		}
 
+		tail--
+	}
 }
 
 func main() {
