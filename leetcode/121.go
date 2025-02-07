fb7d43895e8b999285c1d98f96743e81cb5c4437
joseph sheng <sjjworkspace@gmail.com>
2025-02-07 14:04:22 +0800

    121. 买卖股票的最佳时机(一次遍历)

--- leetcode/121.go
+++ leetcode/121.go
@@ -2,24 +2,21 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func maxProfit(prices []int) int {
-	var lowPrice int
-	var maxProfit int
-
-	lowPrice = prices[0]
-
-	for _, i := range prices {
-		lowPrice = min(lowPrice, i)
-		if i-lowPrice > maxProfit {
-			maxProfit = i - lowPrice
-		}
-
+	minPrice := math.MaxInt
+	maxprofit := 0
+	for _, v := range prices {
+		minPrice = min(minPrice, v)
+		prohit := v - minPrice
+		maxprofit = max(maxprofit, prohit)
 	}
-	return maxProfit
+	return maxprofit
 }
 
 func main() {
 	fmt.Println(maxProfit([]int{7, 1, 5, 3, 6, 4}))
+	fmt.Println(maxProfit([]int{7, 6, 4, 3, 1}))
 }
