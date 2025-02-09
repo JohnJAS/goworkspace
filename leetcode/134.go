b56db78f768959872f2e8e3db0379fc2c4859912
joseph sheng <sjjworkspace@gmail.com>
2025-02-09 23:05:51 +0800

    redo 134

--- leetcode/134.go
+++ leetcode/134.go
@@ -3,28 +3,24 @@ package main
 import "fmt"
 
 func canCompleteCircuit(gas []int, cost []int) int {
-	for i := 0; i < len(gas); i++ {
-		sumOfGas, sumOfCost, offset := 0, 0, 0
-
-		for offset < len(gas) {
-			j := (i + offset) % len(gas)
-			sumOfGas += gas[j]
-			sumOfCost += cost[j]
-
-			if sumOfGas < sumOfCost {
+	length := len(gas)
+	//i++是应为到offset无法到下一跳时，要向后一个
+	for i := 0; i < length; i++ {
+		totalGas, totalCost, offset := 0, 0, 0
+		for offset < length {
+			totalGas += gas[(i+offset)%length]
+			totalCost += cost[(i+offset)%length]
+			//当前的gas无法抵达下一跳时结束
+			if totalGas < totalCost {
 				break
 			}
-
 			offset++
-
 		}
-
-		if offset == len(gas) {
+		if offset == length {
 			return i
 		} else {
-			i = i + offset
+			i += offset
 		}
-
 	}
 	return -1
 }
