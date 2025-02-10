16d268df5a60f15b4b16acbac047beff05acd1f7
joseph sheng <sjjworkspace@gmail.com>
2025-02-10 16:38:55 +0800

    redo 3. 无重复字符的最长子串(滑动窗口)

--- leetcode/3.go
+++ leetcode/3.go
@@ -3,29 +3,32 @@ package main
 import "fmt"
 
 func lengthOfLongestSubstring(s string) int {
-	//record character maps
-	m := map[byte]int{}
-	//define the right pointer(notice from -1)
-	rk := -1
-	//define the length of longest substring
+	n := len(s)
 	maxLength := 0
-	//go through each character in the string
-	for i := 0; i < len(s); i++ {
-		//delete the previous character from map
-		if i > 0 {
-			delete(m, s[i-1])
-		}
-		//increase right pointer to the rightest place
-		for rk+1 < len(s) && m[s[rk+1]] == 0 {
-			rk = rk + 1
-			m[s[rk]] = 1
-		}
-		maxLength = max(maxLength, rk-i+1)
-	}
+	startIndex := 0
+	//<character:index>
+	m := make(map[byte]int)
 
+	for i := 0; i < n; i++ {
+		//检查是否有重复
+		if _, ok := m[s[i]]; ok {
+			//删除到重复字符前的所有记录的字符
+			for startIndex <= m[s[i]] {
+				delete(m, s[startIndex])
+				startIndex++
+			}
+		}
+		//记录当前字符
+		m[s[i]] = i
+		maxLength = max(maxLength, i+1-startIndex)
+	}
 	return maxLength
 }
 
 func main() {
+	fmt.Println(lengthOfLongestSubstring("abba"))
+	fmt.Println(lengthOfLongestSubstring("dvdf"))
 	fmt.Println(lengthOfLongestSubstring("abcabcbb"))
+	fmt.Println(lengthOfLongestSubstring("bbbbb"))
+	fmt.Println(lengthOfLongestSubstring("pwwkew"))
 }
