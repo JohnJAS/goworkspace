2beac56ef694e39fddf47d3ee6fe42cf0186d612
Joseph Sheng <jsheng@opentext.com>
2025-02-11 16:08:27 +0800

    12. 罗马字符转换

---
+++ leetcode/12-2.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+var valueSymbols = []struct {
+	value  int
+	symbol string
+}{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
+func intToRoman2(num int) string {
+	roman := []byte{}
+	for _, vs := range valueSymbols {
+		for num >= vs.value {
+			num -= vs.value
+			roman = append(roman, vs.symbol...)
+		}
+		if num == 0 {
+			break
+		}
+	}
+	return string(roman)
+}
+
+func main() {
+	fmt.Println(intToRoman2(35))
+	fmt.Println(intToRoman2(3749))
+	fmt.Println(intToRoman2(58))
+	fmt.Println(intToRoman2(1994))
+}
--- leetcode/12.go
+++ leetcode/12.go
@@ -1,13 +1,21 @@
 package main
 
-//TODO
-//func intToRoman(num int) string {
-//
-//}
+import "fmt"
+
+var table = [][]string{
+	{"", "M", "MM", "MMM"},
+	{"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"},
+	{"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"},
+	{"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"},
+}
+
+func intToRoman(num int) string {
+	return table[0][num/1000] + table[1][num%1000/100] + table[2][num%100/10] + table[3][num%10]
+}
 
 func main() {
-	//fmt.Println(intToRoman(5))
-	//fmt.Println(intToRoman(3749))
-	//fmt.Println(intToRoman(58))
-	//fmt.Println(intToRoman(1994))
+	fmt.Println(intToRoman(35))
+	fmt.Println(intToRoman(3749))
+	fmt.Println(intToRoman(58))
+	fmt.Println(intToRoman(1994))
 }
