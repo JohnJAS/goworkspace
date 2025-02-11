50101a9daefd1b5ad3d3de5bf0b126dc5405934c
Joseph Sheng <jsheng@opentext.com>
2025-02-11 17:37:18 +0800

    redo 73. 矩阵置零

--- leetcode/73.go
+++ leetcode/73.go
@@ -3,29 +3,27 @@ package main
 import "fmt"
 
 func setZeroes(matrix [][]int) {
-	row := make([]int, len(matrix))
-	col := make([]int, len(matrix[0]))
-
-	for i, r := range matrix {
-		for j, v := range r {
-			if v == 0 {
-				row[i] = 1
-				col[j] = 1
+	rows := make([]bool, len(matrix))
+	cols := make([]bool, len(matrix[0]))
+	for i := 0; i < len(matrix); i++ {
+		for j := 0; j < len(matrix[i]); j++ {
+			if matrix[i][j] == 0 {
+				rows[i] = true
+				cols[j] = true
 			}
 		}
 	}
 
-	for i, r := range matrix {
-		for j := range r {
-			if row[i] == 1 || col[j] == 1 {
-				r[j] = 0
+	for i := 0; i < len(matrix); i++ {
+		for j := 0; j < len(matrix[i]); j++ {
+			if rows[i] || cols[j] {
+				matrix[i][j] = 0
 			}
 		}
 	}
-	fmt.Println(matrix)
 }
-
 func main() {
-	martix := [][]int{{1, 1, 1}, {1, 0, 1}, {1, 1, 1}}
-	setZeroes(martix)
+	matrix := [][]int{{1, 1, 1}, {1, 0, 1}, {1, 1, 1}}
+	setZeroes(matrix)
+	fmt.Println(matrix)
 }
