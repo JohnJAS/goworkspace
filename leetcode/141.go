a1afd95dbf6f8d51dca00f37d4bb9fa7793d1cac
Joseph Sheng <jsheng@opentext.com>
2025-02-12 16:12:59 +0800

    redo 141 快慢执政

--- leetcode/141.go
+++ leetcode/141.go
@@ -1,30 +1,34 @@
 package main
 
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
-func hasCycle(head *ListNode) bool {
-	if head == nil || head.Next == nil {
-		return false
-	}
-	slow := head
-	fast := head.Next
+import "fmt"
 
-	for slow != fast {
-		if slow != nil {
-			slow = slow.Next
-		} else {
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+var p5 = ListNode{5, nil}
+var p4 = ListNode{4, &p5}
+var p3 = ListNode{3, &p4}
+var p2 = ListNode{2, &p3}
+var p1 = ListNode{1, &p2}
+var HEAD = ListNode{1, &p1}
+
+func hasCycle(head *ListNode) bool {
+	fast, slow := head, head
+	for fast != nil {
+		if fast.Next == nil {
 			return false
 		}
-		if fast != nil && fast.Next != nil {
-			fast = fast.Next.Next
-		} else {
-			return false
+		fast = fast.Next.Next
+		slow = slow.Next
+		if fast == slow {
+			return true
 		}
 	}
-	return true
+	return false
+}
+
+func main() {
+	fmt.Println(hasCycle(&HEAD))
 }
