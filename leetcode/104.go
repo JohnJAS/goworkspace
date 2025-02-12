725b3b88995596276630d3c49934ab4dfc5bac6d
Joseph Sheng <jsheng@opentext.com>
2025-02-12 16:29:47 +0800

    redo 104

--- leetcode/104.go
+++ leetcode/104.go
@@ -1,16 +1,24 @@
 package main
 
-/**
- * Definition for a binary tree node.
- * type TreeNode struct {
- *     Val int
- *     Left *TreeNode
- *     Right *TreeNode
- * }
- */
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+var l1 TreeNode = TreeNode{1, nil, nil}
+var l2 TreeNode = TreeNode{2, nil, nil}
+var root TreeNode = TreeNode{3, &l1, &l2}
+
 func maxDepth(root *TreeNode) int {
 	if root == nil {
 		return 0
 	}
 	return max(maxDepth(root.Left), maxDepth(root.Right)) + 1
+}
+
+func main() {
+	fmt.Println(maxDepth(&root))
 }
