172d80b4a0355a0f22316455736c88a9207f6716
Joseph Sheng <jsheng@opentext.com>
2025-02-12 18:00:34 +0800

    2. 两数相加

---
+++ leetcode/2.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+)
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+var l111 = ListNode{3, nil}
+var l11 = ListNode{2, &l111}
+var l1 = ListNode{1, &l11}
+
+var l2 = ListNode{9, nil}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	//进位
+	flag := 0
+	var cur *ListNode
+	//dummy head
+	head := &ListNode{0, nil}
+	pre := head
+	for l1 != nil || l2 != nil {
+		sum := flag
+		if l1 != nil {
+			sum += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			sum += l2.Val
+			l2 = l2.Next
+		}
+
+		flag = sum / 10
+		sum = sum % 10
+
+		//创建当前节点的node
+		cur = &ListNode{
+			sum,
+			nil,
+		}
+		pre.Next = cur
+		//for next round
+		pre = cur
+	}
+	if flag == 1 {
+		cur.Next = &ListNode{1, nil}
+	}
+
+	return head.Next
+}
+
+func main() {
+	fmt.Println(addTwoNumbers(&l1, &l2))
+}
