ce3f75b30f3c5c0eab42406015409d08ed639481
Joseph Sheng <jsheng@opentext.com>
2025-02-13 17:06:29 +0800

    155. 最小栈

---
+++ leetcode/155.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type MinStack struct {
+	stack []int
+	//根据栈先进后出，当前min下面的都比它大，上面的都比它小
+	minStack []int
+}
+
+func Constructor() MinStack {
+	return MinStack{[]int{}, []int{}}
+}
+
+func (this *MinStack) Push(val int) {
+	this.stack = append(this.stack, val)
+	if len(this.minStack) == 0 || this.GetMin() > val {
+		this.minStack = append(this.minStack, min(val, this.GetMin()))
+	}
+}
+
+func (this *MinStack) Pop() {
+	if len(this.stack) == 0 {
+		return
+	}
+	if len(this.minStack) == 0 {
+		return
+	}
+	if this.Top() == this.GetMin() {
+		this.minStack = this.minStack[:len(this.minStack)-1]
+	}
+	this.stack = this.stack[:len(this.stack)-1]
+}
+
+func (this *MinStack) Top() int {
+	if len(this.stack) == 0 {
+		return math.MaxInt
+	}
+	return this.stack[len(this.stack)-1]
+}
+
+func (this *MinStack) GetMin() int {
+	if len(this.minStack) == 0 {
+		return math.MaxInt
+	}
+	return this.minStack[len(this.minStack)-1]
+}
+
+func main() {
+	obj := Constructor()
+	obj.Push(5)
+	obj.Push(5)
+	obj.Push(6)
+	obj.Push(1)
+	fmt.Println(obj.Top())
+	fmt.Println(obj.GetMin())
+	obj.Pop()
+	fmt.Println(obj.Top())
+	fmt.Println(obj.GetMin())
+}
