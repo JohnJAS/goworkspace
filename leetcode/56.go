1ea33e5d7f01178e160bcfb64319a9910feac1b0
joseph sheng <sjjworkspace@gmail.com>
2025-02-15 16:45:16 +0800

    redo 56

--- leetcode/56.go
+++ leetcode/56.go
@@ -7,33 +7,18 @@ import (
 
 func merge2(intervals [][]int) [][]int {
 	var result [][]int
-
-	//sort the intervals
 	sort.Slice(intervals, func(i, j int) bool {
-		return intervals[i][0] < intervals[j][0] || (intervals[i][0] == intervals[j][0] && intervals[i][1] < intervals[j][1])
+		return intervals[i][0] < intervals[j][0]
 	})
-
-	fmt.Println(intervals)
-
 	for i := 0; i < len(intervals); i++ {
-		if i == 0 {
+		if len(result) == 0 || result[len(result)-1][1] < intervals[i][0] {
 			result = append(result, intervals[i])
+			continue
 		}
-		if i > 0 {
-			if result[len(result)-1][0] == intervals[i][0] {
-				result[len(result)-1][1] = intervals[i][1]
-				continue
-			}
-			if result[len(result)-1][1] < intervals[i][0] {
-				result = append(result, intervals[i])
-			} else {
-				if result[len(result)-1][1] <= intervals[i][1] {
-					result[len(result)-1][1] = intervals[i][1]
-				} else {
-					continue
-				}
-			}
+		if result[len(result)-1][1] > intervals[i][1] {
+			continue
 		}
+		result[len(result)-1][1] = intervals[i][1]
 	}
 	return result
 }
