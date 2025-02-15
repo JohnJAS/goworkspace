2c5f945144927470bb03ad079be64ad1cfdbc772
joseph sheng <sjjworkspace@gmail.com>
2025-02-15 17:09:22 +0800

    redo 19

--- leetcode/19.go
+++ leetcode/19.go
@@ -6,20 +6,21 @@ package main
 //}
 
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
-	dummy := &ListNode{0, head}
-
+	dummy := &ListNode{Next: head}
 	fast, slow := head, dummy
 
 	for ; n > 0; n-- {
 		fast = fast.Next
 	}
 
-	for ; fast != nil; fast = fast.Next {
+	for fast != nil {
 		slow = slow.Next
+		fast = fast.Next
 	}
 
 	//delete node
 	slow.Next = slow.Next.Next
 
+	//注意头节点会被删除
 	return dummy.Next
 }
