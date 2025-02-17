6baf8c0a30c06b1f4d3287dbb752a859cae50582
joseph sheng <sjjworkspace@gmail.com>
2025-02-17 20:40:37 +0800

    226. 翻转二叉树

--- leetcode/226.go
+++ leetcode/226.go
@@ -1,27 +1,36 @@
 package main
 
-/**
-* Definition for a binary tree node.
-* type TreeNode struct {
-*     Val int
-*     Left *TreeNode
-*     Right *TreeNode
-* }
- */
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
 	Right *TreeNode
 }
 
+func (t *TreeNode) addLeft(val int) {
+	t.Left = &TreeNode{Val: val}
+}
+
+func (t *TreeNode) addRight(val int) {
+	t.Right = &TreeNode{Val: val}
+}
 func invertTree(root *TreeNode) *TreeNode {
 	if root == nil {
-		return nil
+		return root
 	}
-	// get new left and new right
-	left := invertTree(root.Left)
-	right := invertTree(root.Right)
-	root.Left = right
-	root.Right = left
+	root.Left, root.Right = invertTree(root.Right), invertTree(root.Left)
 	return root
+}
+
+func main() {
+	root := &TreeNode{Val: 5}
+	root.addLeft(4)
+	root.Left.addLeft(11)
+	root.Left.Left.addLeft(7)
+	root.Left.Left.addRight(2)
+	root.addRight(8)
+	root.Right.addLeft(13)
+	root.Right.addRight(4)
+	root.Right.Right.addRight(1)
+
+	invertTree(root)
 }
