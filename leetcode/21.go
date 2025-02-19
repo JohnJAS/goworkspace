36ffb0e02c352181bb6d0f66b797145a793e7f5c
Joseph Sheng <jsheng@opentext.com>
2025-02-19 13:24:20 +0800

    redo 21,不使用list2插入list1,直接建dummy，next下去

--- leetcode/21.go
+++ leetcode/21.go
@@ -31,24 +31,23 @@ func (l *ListNode) print() {
 }
 
 func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
-	dummy := &ListNode{0, list1}
-	pre := dummy
-
+	dummy := &ListNode{}
+	cur := dummy
 	for list1 != nil && list2 != nil {
-		if list1.Val > list2.Val {
-			pre.Next = list2
-			tempList2 := list2
-			list2 = list2.Next
-			tempList2.Next = list1
-			pre = tempList2
-		} else {
-			pre = list1
+		if list1.Val < list2.Val {
+			cur.Next = list1
+			cur = cur.Next
 			list1 = list1.Next
+		} else {
+			cur.Next = list2
+			cur = cur.Next
+			list2 = list2.Next
 		}
 	}
-
-	if list2 != nil {
-		pre.Next = list2
+	if list1 != nil {
+		cur.Next = list1
+	} else {
+		cur.Next = list2
 	}
 
 	return dummy.Next
@@ -60,16 +59,15 @@ func main() {
 	list1.add(4)
 	list1.add(7)
 	list1.add(9)
+	list1.print()
 
 	list2 := &ListNode{1, nil}
 	list2.add(2)
 	list2.add(9)
 	list2.add(11)
 	list2.add(12)
+	list2.print()
 
 	test := mergeTwoLists(list1, list2)
-	for test != nil {
-		fmt.Println(test.Val)
-		test = test.Next
-	}
+	test.print()
 }
