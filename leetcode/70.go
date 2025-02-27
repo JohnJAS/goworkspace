536151163d1106dc785178eb269815ee39fcf58c
Joseph Sheng <jsheng@opentext.com>
2025-02-27 16:27:01 +0800

    enhance 70. 爬楼梯

--- leetcode/70.go
+++ leetcode/70.go
@@ -10,6 +10,21 @@ func climbStairs(n int) int {
 	return climbStairs(n-1) + climbStairs(n-2)
 }
 
+func climbStairs2(n int) int {
+
+	if n < 3 {
+		return n
+	}
+	prePre, pre := 1, 2
+	var result int
+	for i := 3; i <= n; i++ {
+		result = pre + prePre
+		prePre = pre
+		pre = result
+	}
+	return result
+}
+
 func main() {
 	fmt.Println(climbStairs(5))
 }
