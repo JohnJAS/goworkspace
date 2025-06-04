dcb1715565f6f385eee6cb11f68d01db43696739
joseph sheng <sjjworkspace@gmail.com>
2025-06-04 22:28:41 +0800

    add comments

--- leetcode/25.go
+++ leetcode/25.go
@@ -29,44 +29,46 @@ func (l *ListNode) print() {
 }
 
 func reverseKGroup(head *ListNode, k int) *ListNode {
-	dummy := &ListNode{Next: head}
-	pre := dummy
-	cur := head
+	//函数框架与初始化
+	dummy := &ListNode{Next: head} // 创建哨兵节点
+	pre := dummy                   // pre 指向上一段的尾部，初始为 dummy
+	cur := head                    // cur 指向当前段的头部
 
+	//外层循环：逐组处理
 	for cur != nil {
-		tail := pre
-		for i := 0; i < k; i++ {
+		tail := pre              // tail 初始化为 pre，为了下面迭代找到tail，确定[cur,...,tail]的区间，如果从cur往后推，tail会变成tail.Next
+		for i := 0; i < k; i++ { // 向后走 k 步找本组的尾
 			tail = tail.Next
-			if tail == nil {
+			if tail == nil { // 不足 k 个，直接返回结果
 				return dummy.Next
 			}
 		}
 
-		next := tail.Next
-		cur, tail = reverse(cur, tail)
+		//存下一组起点 + 反转本组
+		next := tail.Next              // next 记录下一组的起点
+		cur, tail = reverse(cur, tail) // 反转 [cur, tail]，返回新的头尾
 
-		// 重新连接链表
-		pre.Next = cur
-		tail.Next = next
+		//把反转后的子链接回主链(链表题的“灵魂两行”)
+		pre.Next = cur   // pre → 新头,把上一组（或 dummy）连到这组的新头；
+		tail.Next = next // 新尾 → next（下一组的头）,把这组的新尾连回下一组的头，从而保持全链表连通。
 
-		// 更新指针，准备下一组
-		pre = tail
-		cur = tail.Next
+		//更新指针，开始下一轮
+		pre = tail      // pre 移到当前组的新尾,作为下一组的pre
+		cur = tail.Next // cur 移到下一组的头
 	}
-
 	return dummy.Next
 }
 
 func reverse(head, tail *ListNode) (*ListNode, *ListNode) {
-	pre := tail.Next
-	cur := head
-	for pre != tail {
+	pre := tail.Next  // pre 初始指向尾节点之后的位置（可能是 nil）
+	cur := head       // cur 从 head 开始遍历到 tail
+	for pre != tail { // 当 pre 走到 tail 才结束
 		next := cur.Next
-		cur.Next = pre
-		pre = cur
+		cur.Next = pre // 反转指针
+		pre = cur      // pre 与 cur 双指针前进
 		cur = next
 	}
-	return tail, head
+	return tail, head // 反转后新的头是旧 tail，新尾是旧 head，不可以用cur,因为cur已经迭代下去了
 }
 
 func main() {
